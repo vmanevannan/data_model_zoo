a00e170eb176975ccda344546d213efb5cfc9f19
vmanevannan <vmanevannan@toromontcat.com>
2024-04-29 14:10:11 -0400

    removed dupes

--- base_table.sql
+++
@@ -1,8 +0,0 @@
---Once the catalog and schemas are in place, we create the base table where the raw data will be stored.
-CREATE TABLE product_catalog.general.products (
-    product_id INT,
-    product_name STRING,
-    dept STRING,
-    sales DOUBLE,
-    ad_clicks INT
-);
